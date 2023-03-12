ac43d29c850470379304916040ee974d92047c38
ilyas-Rk <59224491+ilyas-Rk@users.noreply.github.com>
2023-03-12 20:23:10 +0100

    25/02 Modification Classe Effect V1

--- src/main/Effect.java
+++ src/main/Effect.java
@@ -1,5 +1,59 @@
 package main;
 
 public class Effect {
-    
+    protected TypeEffect typeEffect;
+    protected TypeJauge nomJauge;
+    protected int valeur;
+    public Effect(TypeEffect typeEffect, TypeJauge nomJauge, int valeur) {
+        this.typeEffect = typeEffect;
+        this.nomJauge = nomJauge;
+        this.valeur = valeur;
+    }
+
+
+    public void setNomJauge(TypeJauge nomJauge) {
+        this.nomJauge = nomJauge;
+    }
+
+    public void setValeur(int valeur) {
+        this.valeur = valeur;
+    }
+
+    public TypeEffect getTypeEffect() {
+        return typeEffect;
+    }
+
+    public TypeJauge getNomJauge() {
+        return nomJauge;
+    }
+
+    public int getValeur() {
+        return valeur;
+    }
+
+    @Override
+    public String toString() {
+        return "; jauge " +
+                 nomJauge +
+                 (valeur>0 ? "+":"") +
+                valeur;
+    }
+    public void appilquer(Personnage personnage){
+        if(personnage.jaugeFinance.type==this.nomJauge){
+            personnage.jaugeFinance.valeur += this.getValeur();
+            return;
+        }
+        if(personnage.jaugeArmee.type==this.nomJauge){
+            personnage.jaugeArmee.valeur += this.getValeur();
+            return;
+        }
+        if(personnage.jaugePeuple.type==this.nomJauge){
+            personnage.jaugePeuple.valeur += this.getValeur();
+            return;
+        }
+        if(personnage.jaugeFinance.type==this.nomJauge){
+            personnage.jaugeFinance.valeur += this.getValeur();
+        }
+    }
+
 }
