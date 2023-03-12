17e887c559225957a14db8bf3750a34559275207
ilyas-Rk <59224491+ilyas-Rk@users.noreply.github.com>
2023-03-12 20:33:32 +0100

    06/03 Mise a jour Classe Personnage V2

--- src/main/Personnage.java
+++ src/main/Personnage.java
@@ -1,6 +1,5 @@
 package main;
 
-
 public class Personnage {
 
     protected String nom;
@@ -18,54 +17,19 @@ public class Personnage {
     public Personnage(String nom, Genre genre) {
         this.nom = nom;
         this.genre = genre;
-
-        
-        jaugeClerge = new Jauge("Clergé", 15 + (int)(Math.random() * (35 - 15)));
-        jaugePeuple = new Jauge("Peuple", 15 + (int)(Math.random() * (35 - 15)));
-        jaugeArmee = new Jauge("Armée", 15 + (int)(Math.random() * (35 - 15)));
-        jaugeFinance = new Jauge("Finances", 15 + (int)(Math.random() * (35 - 15)));
+        // Initialisation des jauges entre 15 et 35 points
+        jaugeClerge = new Jauge(TypeJauge.CLERGE,"Clergé", 15 + (int)(Math.random() * (35 - 15)));
+        jaugePeuple = new Jauge(TypeJauge.PEUPLE, "Peuple", 15 + (int)(Math.random() * (35 - 15)));
+        jaugeArmee = new Jauge(TypeJauge.ARMEE, "Armée", 15 + (int)(Math.random() * (35 - 15)));
+        jaugeFinance = new Jauge(TypeJauge.FINANCE, "Finances", 15 + (int)(Math.random() * (35 - 15)));
     }
-
 
     public void AfficheJauges() {
-        afficheJauge(jaugeClerge);
-        afficheJauge(jaugePeuple);
-        afficheJauge(jaugeArmee);
-        afficheJauge(jaugeFinance);
+        jaugeClerge.afficheJauge();
+        jaugePeuple.afficheJauge();
+        jaugeArmee.afficheJauge();
+        jaugeFinance.afficheJauge();
         System.out.println();
-    }
-
-
-    public boolean finDuJeu(){
-        if(jaugeClerge.getValeur()<=0
-                || jaugeClerge.getValeur()>=50
-                || jaugePeuple.getValeur()<=0
-                || jaugePeuple.getValeur()>=50
-                || jaugeArmee.getValeur()<=0
-                || jaugeArmee.getValeur()>=50
-                || jaugeFinance.getValeur()<=0
-                || jaugeFinance.getValeur()>=50){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-
-    private void afficheJauge(Jauge jauge) {
-        String resultat = "[";
-
-        for(int i=0;i<jauge.getValeur();i++){
-            resultat += "#";
-        }
-
-        for(int i=0;i<50-(jauge.getValeur()>0?jauge.getValeur():0);i++){
-            resultat += "_";
-        }
-        resultat += "] ";
-
-        resultat += jauge.getNom();
-        System.out.println(resultat);
     }
 
 
@@ -89,7 +53,6 @@ public class Personnage {
         return jaugeClerge;
     }
 
-
     public void setJaugeClerge(Jauge jaugeClerge) {
         this.jaugeClerge = jaugeClerge;
     }
@@ -111,7 +74,6 @@ public class Personnage {
     public void setJaugeArmee(Jauge jaugeArmee) {
         this.jaugeArmee = jaugeArmee;
     }
-
 
     public Jauge getJaugeFinance() {
         return jaugeFinance;
